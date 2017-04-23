0ab11a8c6e7cd0accedb0476c89fd515576f0308
skycore9 <skycore@163.com>
2017-04-23 12:08:18 +0800

    rm

--- kd-trees/parSort.h
+++
@@ -1,4 +0,0 @@
-void sampleSort(float * a, int *index, int n);
-void seqShellSort(float *a, int *index, int n);
-void seqShellSort(float *a, int n);
-
--- kd-trees/rerank.h
+++
@@ -1,5 +0,0 @@
-int parallel_find_max (int* ranks, int size);
-void scan (int* aux_scan, int size);
-int find_max(int* ranks, int size);
-void rerank(int* ranks, int size);
-
