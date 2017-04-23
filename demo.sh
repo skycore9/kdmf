d958d258672b25bb89efa2a27a37aedbfae96536
skycore9 <skycore@163.com>
2017-04-23 12:17:11 +0800

    add

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./kdmf --build-index smalldata smalldata.tr.bin
-./kdmf --build-index smalldata smalldata.te.bin
-./kdmf --train --tr-rmse --obj -k 40 -s 4 -p 0.05 -q 0.05 -g 0.003 -v smalldata.te.bin smalldata.tr.bin model
-./kdmf --predict smalldata.te.bin model output
+./kdmf --build-index dataset dataset_tr_index
+./kdmf --build-index dataset dataset_te_index
+./kdmf --train --tr-rmse --obj -k 40 -s 4 -p 0.05 -q 0.05 -g 0.003 -v dataset_te_index dataset_tr_index model
+./kdmf --predict dataset_te_index model output
